965a3a1ae1c5682be4008417a6638e23eacd856b
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-14 20:04:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,18 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert
+def using_insert(array, element)
+  array.insert
 end
 
 def using_uniq
+end
+
+def using_flatten
+end
+
+def using_delete
+end
+
+def using_delete_at
 end
